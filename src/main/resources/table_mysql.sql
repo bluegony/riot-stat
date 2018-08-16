d48681a5bc5bb5188385cdd2da5ae054a90cd397
bluegony <bluegony@sk.com>
2018-08-16 14:07:24 +0900

    riot_stat query

--- src/main/resources/table_mysql.sql
+++ src/main/resources/table_mysql.sql
@@ -1,22 +1,37 @@
 -- mysql
-create table tempc
+CREATE TABLE TEMPC
 (
-    id int ,
-    name varchar(25 )
+    ID INT ,
+    NAME VARCHAR(25)
 );
 CREATE TABLE TEMP_STAT
 (
-    ACCOUNT_ID int,
-    CURRENT_ACCOUNT_ID int,
-    SID int,
-    GID bigint, -- or long(=text type)
-    WIN int,
-    DURATION int,
-    SEASON int,
-    VERSION VARCHAR(20),
-    CREATED_DATE DATE,
+    GID BIGINT,
+    SID INT,
     START_TIME TIMESTAMP (6),
-    CID int,
-    OWNER int,
-    LOSE int
+    SUMMONER_NAME VARCHAR(20),
+    OWNER INT,
+    CID INT,
+    CHAMPION_NAME VARCHAR(30),
+    WIN INT,
+    LOSE INT,
+    DURATION INT,
+    CREATED_DATE DATE,
+    ACCOUNT_ID INT,
+    CURRENT_ACCOUNT_ID INT,
+    SEASON INT,
+    VERSION VARCHAR(20),
+
+    PRIMARY KEY (GID, SID)
 );
+
+SELECT
+    CHAMPION_NAME,
+    SUM(WIN)+SUM(LOSE) TOTAL,
+    SUM(WIN) WIN,
+    SUM(LOSE) LOSE,
+    ROUND(SUM(WIN)/(SUM(WIN)+SUM(LOSE)),4) RATE
+FROM TEMP_STAT S
+WHERE OWNER=1
+GROUP BY CHAMPION_NAME
+ORDER BY WIN DESC,LOSE
